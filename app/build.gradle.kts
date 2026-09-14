162571879062a5133864c3b96ff07524907f3bca
chr0nzz <187675356+chr0nzz@users.noreply.github.com>
2026-09-14 18:50:11 -0400

    chore: start v2.4.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -25,8 +25,8 @@ android {
         applicationId = "dev.chr0nzz.traefikmanager"
         minSdk = 33
         targetSdk = 37
-        versionCode = 29
-        versionName = "2.3.0"
+        versionCode = 30
+        versionName = "2.4.0"
     }
 
     signingConfigs {
